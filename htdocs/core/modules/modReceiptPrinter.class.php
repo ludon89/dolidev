c82a709ff3d34bab6051ea941e593f76be1aca6d
Laurent Destailleur <eldy@destailleur.fr>
2025-10-22 15:27:48 +0200

    FIX Printers can have duplicate names

--- htdocs/core/modules/modReceiptPrinter.class.php
+++ htdocs/core/modules/modReceiptPrinter.class.php
@@ -146,6 +146,8 @@ class modReceiptPrinter extends DolibarrModules
 		$sql = array(
 			"CREATE TABLE IF NOT EXISTS ".MAIN_DB_PREFIX."printer_receipt (rowid integer AUTO_INCREMENT PRIMARY KEY, name varchar(128), fk_type integer, fk_profile integer, parameter varchar(128), entity integer) ENGINE=innodb;",
 			"CREATE TABLE IF NOT EXISTS ".MAIN_DB_PREFIX."printer_receipt_template (rowid integer AUTO_INCREMENT PRIMARY KEY, name varchar(128), template text, entity integer) ENGINE=innodb;",
+			"ALTER TABLE ".MAIN_DB_PREFIX."printer_receipt ADD UNIQUE INDEX uk_printer_receipt (name, entity);",
+
 			"DELETE FROM ".MAIN_DB_PREFIX."printer_receipt_template WHERE name = '".$this->db->escape($langs->trans('Example'))."';",
 			"INSERT INTO ".MAIN_DB_PREFIX."printer_receipt_template (name,template,entity) VALUES ('".$this->db->escape($langs->trans('Example'))."', '".$this->db->escape($templateexample)."', 1);",
 		);
