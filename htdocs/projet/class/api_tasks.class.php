9b0732dd49161988bedb38ea727e0e32ef60f0d1
Jon Bendtsen <xcodeauthor@jonb.dk>
2025-11-30 00:02:34 +0100

    FIX: #33899 API Timespent Service Field

--- htdocs/projet/class/api_tasks.class.php
+++ htdocs/projet/class/api_tasks.class.php
@@ -78,7 +78,7 @@ class Tasks extends DolibarrApi
 			throw new RestException(404, 'Task not found');
 		}
 
-		if (!DolibarrApi::_checkAccessToResource('task', $this->task->id)) {
+		if (!DolibarrApi::_checkAccessToResource('task', $this->task->id, 'projet_task')) {
 			throw new RestException(403, 'Access not allowed for login '.DolibarrApiAccess::$user->login);
 		}
 
