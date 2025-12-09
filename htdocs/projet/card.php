39baead02d5730aff107c09018c2c2d9dfd17380
Frédéric FRANCE <frederic.france@free.fr>
2025-12-09 10:45:06 +0100

    fix read property client on null

--- htdocs/projet/card.php
+++ htdocs/projet/card.php
@@ -364,7 +364,7 @@ if (empty($reshook)) {
 				setEventMessages($langs->trans("ErrorOppStatusRequiredIfAmount"), null, 'errors');
 			}
 
-			if (!$error && !is_null($object->thirdparty) && (int) $object->thirdparty->client == 0 || (int) $object->thirdparty->client == 2) {		// If not yet customer
+			if (!$error && !is_null($object->thirdparty) && ((int) $object->thirdparty->client == 0 || (int) $object->thirdparty->client == 2)) {		// If not yet customer
 				// Get ID of the special opportunity status code 'WON'
 				$idoppstatuswon = (int) dol_getIdFromCode($db, 'WON', 'c_lead_status', 'code', 'rowid');
 
