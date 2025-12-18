86328e840eaf01a8e57e193e1edca804463bf2b6
William Mead <william@m34d.com>
2025-12-18 11:25:31 +0100

    Cleaned code

--- htdocs/api/class/api_documents.class.php
+++ htdocs/api/class/api_documents.class.php
@@ -601,7 +601,6 @@ class Documents extends DolibarrApi
 				throw new RestException(404, 'Event not found');
 			}
 
-			$upload_dir = getMultidirOutput($object) . '/'.dol_sanitizeFileName($object->ref);
 			$upload_dir = $conf->agenda->multidir_output[$object->entity ?? $conf->entity].'/'.get_exdir(0, 0, 0, 1, $object, 'agenda');
 		} elseif ($modulepart == 'expensereport') {
 			require_once DOL_DOCUMENT_ROOT.'/expensereport/class/expensereport.class.php';
