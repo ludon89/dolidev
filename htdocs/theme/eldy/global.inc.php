a5dd3cf63b465f83ac6d24577cfa934ca7c6b44f
Laurent Destailleur <eldy@users.sourceforge.net>
2026-01-15 02:44:23 +0100

    Fix css regression

--- htdocs/theme/eldy/global.inc.php
+++ htdocs/theme/eldy/global.inc.php
@@ -4153,6 +4153,10 @@ div.tabBar tr.titre td {
 div.fiche table:not(.table-fiche-title) tr.titre td {
 	padding-top: 10px;
 }
+div.fiche >.table-fiche-title tr.toptitle td.col-title,
+div.fiche >form >.table-fiche-title tr.toptitle td.col-title {
+	filter: grayscale(100%);
+}
 
 div.tabBar.tabBarNoTop {
 	padding-top: 0;
