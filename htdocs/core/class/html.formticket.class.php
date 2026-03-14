38d1b12bfe4501db1cf8cf27d1eb2d33eacdf563
Laurent Destailleur <eldy@destailleur.fr>
2026-03-14 19:20:59 +0100

    Fix CI

--- htdocs/core/class/html.formticket.class.php
+++ htdocs/core/class/html.formticket.class.php
@@ -1718,9 +1718,9 @@ class FormTicket
 				}
 
 				if (!empty($ticketstat->origin_replyto) && !in_array($ticketstat->origin_replyto, $sendto)) {
-					$sendto[] = dol_escape_htmltag($ticketstat->origin_replyto).' <small class="opacitymedium">('.$langs->trans("TicketEmailOriginIssuer").")</small>";
+					$sendto[] = dol_escape_htmltag((string) $ticketstat->origin_replyto).' <small class="opacitymedium">('.$langs->trans("TicketEmailOriginIssuer").")</small>";
 				} elseif ($ticketstat->origin_email && !in_array($ticketstat->origin_email, $sendto)) {
-					$sendto[] = dol_escape_htmltag($ticketstat->origin_email).' <small class="opacitymedium">('.$langs->trans("TicketEmailOriginIssuer").")</small>";
+					$sendto[] = dol_escape_htmltag((string) $ticketstat->origin_email).' <small class="opacitymedium">('.$langs->trans("TicketEmailOriginIssuer").")</small>";
 				}
 
 				if ($ticketstat->fk_soc > 0) {
