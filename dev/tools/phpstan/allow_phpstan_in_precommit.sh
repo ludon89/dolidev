d6db92d3238f1539ceb4d90bb22e282319989752
Laurent Destailleur <eldy@destailleur.fr>
2026-04-27 14:24:15 +0200

    Merge branch 'develop' of git@github.com:Dolibarr/dolibarr.git into develop

--- dev/tools/phpstan/allow_phpstan_in_precommit.sh
+++ dev/tools/phpstan/allow_phpstan_in_precommit.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+# Wrapper to run 'PHPStan' from pre-commit hook
+# This is very slow so not enabled by default
+# To enable it, create a file ~/.run-phpstan
+# To disable it, remove this file ~/.run-phpstan
+
+echo "Running PHPStan on files ~/vendor/bin/phpstan --level=9 -v analyze -a dev/build/phpstan/bootstrap.php $@"
 
 # Test presence of file
-if [ ! -f ".run-phpstan" ]; then
-	echo "Skipping PHPStan (file .run-phpstan missing)"
+if [ ! -f ~/.run-phpstan ]; then
+	echo "Skipping PHPStan (file ~/.run-phpstan missing)"
 	exit 0
 fi
 
-if [ ! -f "~/vendor/bin/phpstan" ]; then
+if [ ! -f ~/vendor/bin/phpstan ]; then
 	echo "Skipping PHPStan (file ~/vendor/bin/phpstan missing)"
 	exit 0
 fi
 
-echo "Running PHPStan"
-~/vendor/bin/phpstan analyz  e
+~/vendor/bin/phpstan --level=9 -v analyze -a dev/build/phpstan/bootstrap.php $@
 
-exit 1;
+exit $?
