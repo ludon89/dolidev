ce1156eb2567127b3dcebbbc136c3fb89545b29e
Laurent Destailleur <eldy@destailleur.fr>
2026-05-27 23:57:01 +0200

    Fix warning

--- htdocs/blockedlog/ajax/block-info.php
+++ htdocs/blockedlog/ajax/block-info.php
@@ -223,7 +223,11 @@ function formatObject($objtoshow, $prefix, $parentelement = '')
 
 				// Field code
 				$s .= '<td>';
-				$s .= '<!-- '.$key.' '.$arrayoffields[$key]['type'].''.$arrayoffields[$convertkey[$key]]['label'].' -->';
+				$types = $arrayoffields[$key]['type'] ?? '';
+				$labels = (array_key_exists($key, $convertkey) ? ($arrayoffields[$convertkey[$key]]['label'] ?? '') : '');
+				$s .= '<!-- '.$key.' '.$types.' '.$labels.' -->';
+
+				$s .= '<!-- '.$key.' '.$arrayoffields[$key]['type'].' '.$arrayoffields[$convertkey[$key]]['label'].' -->';
 				$s .= ($prefix ? $prefix.' > ' : '');
 				$s .= $key;
 				$s .= '</td>';
